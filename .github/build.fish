f19dfd080b4ab7adbc4013dffdddb86bcb912bde
Krishna Manoj PVR <119659348+krishnamanojpvr@users.noreply.github.com>
2025-07-19 19:36:18 +0530

    Update build.fish

--- .github/build.fish
+++ .github/build.fish
@@ -2,17 +2,11 @@
 set filename build.config.json
 
 function buildRepo
-
     set name (basename $argv[1])
     set subdomain (jq -r ".[\"$name\"].subdomain" $filename)
     set repolink (jq -r ".[\"$name\"].repolink" $filename)
     set repodir ./buildfiles/$name
-
-    if test $name = "root"
-        set branch "working"
-    else
-        set branch "main"
-    end
+    set branch "main"   # always use main branch
 
     if test ! -e site
         mkdir site
@@ -25,28 +19,26 @@ function buildRepo
         rm -rf ./buildfiles/*
     end
 
-    echo $name
-    echo $repolink
-    echo $repodir
-    echo "$subdomain $repolink"
+    echo "Cloning repo: $repolink into $repodir on branch $branch"
     set authrepolink (string replace "https://" "https://$PA_TOKEN:@" $repolink)
     git clone -b $branch "$authrepolink" "$repodir"
+
     cd $repodir
     bun i
     bun run build
     cd -
+
     mkdir -p "./site/$subdomain"
     if test -e $repodir/build
         cp -r $repodir/build/* ./site/$subdomain
     else if test -e $repodir/dist
         cp -r $repodir/dist/* ./site/$subdomain
     end
+
     rm -r buildfiles
 end
 
 if test "$argv[1]" = "all"
-
-    set filename build.config.json
     set keys (jq -r 'keys_unsorted | .[]' $filename)
     for i in $keys
         buildRepo "$i"
